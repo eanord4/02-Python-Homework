948fdf3830de434322ab45ba581c2a96bf1d7831
Eric Nordstrom <eanord4@gmail.com>
2019-11-29 09:34:49 -0600

    Add analysis function and main section to PyParagraph/main.py--not yet tested

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -3,14 +3,15 @@
 # Second homework assignment for UT Data Analysis & Visualization boot camp
 # Python 3.6.4
 
+# "On The Road" is a novel by Jack Kerouac that was originally typed without paragraphing. Therefore, it can be considered one of the longest paragraphs ever written. The published version, used here, uses paragraphs, so this script will clean it to put it into single-paragraph form. Disclaimer: the final result will not actually match the original, single-paragraph version as edits were made before publishing.
+# Source: http://www.pauladaunt.com/books/Kerouac,%20Jack%20-%20On%20The%20Road.txt
 
-import re, os
+
+import re
 
 
 """DATA"""
 
-# "On The Road" is a novel by Jack Kerouac that was originally typed without paragraphing. Therefore, it can be considered one of the longest paragraphs ever written. The published version, used here, uses paragraphs, so this script will clean it to put it into single-paragraph form. Disclaimer: the final result will not match the original, single-paragraph version as edits were made before publishing.
-# source: http://www.pauladaunt.com/books/Kerouac,%20Jack%20-%20On%20The%20Road.txt
 original_text_path = 'C:/Users/eanor.LORDSTROM/Dropbox/Documents/Education/Data Analysis & Viz Boot Camp/Homework/02-Python/PyParagraph/on_the_road.txt'  # path on my computer; change to yours
 new_text_path = 'single_paragraph.txt'  # output file path
 
@@ -28,10 +29,45 @@ def clean(in_file_path, out_file_path):
     include = False
 
     with open(in_file_path, 'r') as in_file, open(out_file_path, 'w') as out_file:  # writes over existing output file if present
+
         for para in in_file:
 
-            if para == 'ON THE ROAD\n':
+            if para == 'ON THE ROAD\n':  # start of text to be included
                 include = True
 
             if include:
-                out_file.write(para + '\n')
+                out_file.write(para[:-1] + ' ')  # replace newline with space
+
+def analyze(paragraph_file_path):
+    '''analyze(paragraph_file_path) --> print an analysis of the paragraph'''
+
+    with open(paragraph_file_path, 'r') as para_file:
+        sents = [sent.split() for sent in sentences(para_file.read())]
+
+    n_sent = n_word = n_char = 0
+    
+    for sent in sents:
+
+        for word in sent:
+            n_char += len(word)
+            n_word += 1
+        
+        n_sent += 1
+
+    avg_sent_len = n_word / n_sent
+    avg_letter_count = n_char / n_word
+
+    print('Paragraph Analysis')
+    print('------------------')
+    print('Approximate Word Count:', n_word)
+    print('Approximate Sentence Count:', n_sent)
+    print('Average Letter Count:', avg_letter_count)
+    print('Average Sentence Length:', avg_sent_len)
+
+
+"""RUN"""
+
+if __name__ == '__main__':
+    print()
+    clean(original_text_path, new_text_path)  # create single paragraph file
+    analyze(new_text_path)
